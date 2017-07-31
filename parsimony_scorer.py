0b1dd9d73758478e9b90716803684cebcba964cf
lschachter <lillieschachter@gmail.com>
2017-07-31 15:51:28 -0400

    Update privacy (cuz I know more now)

--- parsimony_scorer.py
+++ parsimony_scorer.py
@@ -12,16 +12,16 @@ class Parsimony_Scorer:
 		'''Opens the newick and nexus files and prepares data for analysis'''
 		newick_file = open(newick_file,'rU')
 		#assumes the newick file is formatted with each tree on its own line
-		self.__newick_trees = newick_file.readlines()
+		self._newick_trees = newick_file.readlines()
 
-		self.__nexus_file_name = nexus_file
+		self._nexus_file_name = nexus_file
 		nexus_file = AlignIO.read(open("nexus_files/"+nexus_file),
 			"nexus")
-		self.__nexus_matrix, self.__nexus_taxa_order = self.__set_chars_and_taxa_order(nexus_file)
+		self._nexus_matrix, self._nexus_taxa_order = self._set_chars_and_taxa_order(nexus_file)
 
-		self.__scored_trees = {}
+		self._scored_trees = {}
 
-	def __set_chars_and_taxa_order(self,nexus_file):
+	def _set_chars_and_taxa_order(self,nexus_file):
 		'''Sets the character matrix and taxa order from the nexus file'''
 		nex_matrix = []
 		nex_taxa = []
@@ -34,64 +34,64 @@ class Parsimony_Scorer:
 	def update_taxa_names(self,taxa_names):
 		'''If your newick-file taxa names don't match those in the nexus
 		file, give the newick-file names here'''
-		self.__nexus_taxa_order = taxa_names
+		self._nexus_taxa_order = taxa_names
 
 	def run(self):
 		'''Runs the scorer over the data'''
-		print("Finding best tree for " + self.__nexus_file_name + "...")
-		self.__score_all_trees()
-		best_tree = self.__find_best_tree()
+		print("Finding best tree for " + self._nexus_file_name + "...")
+		self._score_all_trees()
+		best_tree = self._find_best_tree()
 		return best_tree
 
-	def __score_all_trees(self):
+	def _score_all_trees(self):
 		'''Scores all the trees against the dataset and collects the data
 		in the dictionary self.scored_trees. The dictionary is keyed by score'''
-		for newick in self.__newick_trees:
-			score = self.__score_tree(newick)
-			self.__scored_trees[score] = newick
+		for newick in self._newick_trees:
+			score = self._score_tree(newick)
+			self._scored_trees[score] = newick
 
-	def __score_tree(self, newick):
+	def _score_tree(self, newick):
 		'''Scores a single tree against the entire dataset'''
 		tree = Parsimony_Tree(newick)
 
-		num_cols = len(self.__nexus_matrix[0])
+		num_cols = len(self._nexus_matrix[0])
 		total_parsimony_score = 0
 		for column in range(num_cols):
-			char_dict = self.__get_character_dict(column)
+			char_dict = self._get_character_dict(column)
 			tree.add_leaf_states(char_dict)
 			node_list = tree.get_post_order_nodes()
-			total_parsimony_score += self.__fitch_bottom_up(node_list)
+			total_parsimony_score += self._fitch_bottom_up(node_list)
 		return total_parsimony_score
 
-	def __get_character_dict(self, column):
+	def _get_character_dict(self, column):
 		'''Returns a dictionary of taxa-name and taxa-character
 		to be sent to the tree's add_leaf_states method.
 		Adapted from code by Carolyn Sy'''
-		chars = self.__nexus_matrix[:,column]
-		char_dict = dict(zip(self.__nexus_taxa_order, chars))
+		chars = self._nexus_matrix[:,column]
+		char_dict = dict(zip(self._nexus_taxa_order, chars))
 		return char_dict
 
-	def __fitch_bottom_up(self, node_list):
+	def _fitch_bottom_up(self, node_list):
 		'''Scores the tree using the bottom-up half of Fitch's Algorithm'''
 		parsimony_score = 0
 		for node in node_list:
 			if not node.is_leaf():
-				parsimony_score += self.__set_internal_state(node)
+				parsimony_score += self._set_internal_state(node)
 		return parsimony_score
 
-	def __set_internal_state(self,node):
+	def _set_internal_state(self,node):
 		'''Sets the state options for internal nodes'''
 		l_state = node.left.state
 		r_state = node.right.state
-		intersection = self.__get_intersection(l_state, r_state)
+		intersection = self._get_intersection(l_state, r_state)
 		if intersection:
 			node.state = intersection
 			return 0
-		union = self.__get_union(l_state, r_state)
+		union = self._get_union(l_state, r_state)
 		node.state = union
 		return 1
 	
-	def __get_intersection(self, l_state,r_state):
+	def _get_intersection(self, l_state,r_state):
 		'''takes in two node-states (either strings or lists)
 		returns a list of the intersection of the states, or the
 		state itself if there is only one item'''
@@ -100,7 +100,7 @@ class Parsimony_Scorer:
 			return intersection[0]
 		return intersection
 
-	def __get_union(self, l_state,r_state):
+	def _get_union(self, l_state,r_state):
 		'''takes in two node-states (either strings or lists)
 		returns a list of the union of the states, or the
 		state itself if there is only one item'''
@@ -109,13 +109,13 @@ class Parsimony_Scorer:
 			return union[0]
 		return union
 
-	def __find_best_tree(self):
+	def _find_best_tree(self):
 		'''Finds and prints the best score and best scored tree.
 		NOTE: if there are multiple trees with the same best score,
 		you may not get the same tree returned each time.'''
-		best_score = min(self.__scored_trees.keys())
+		best_score = min(self._scored_trees.keys())
 		print("Best Parsimony score:",best_score)
-		return self.__scored_trees[best_score]
+		return self._scored_trees[best_score]
 
 def main():
 	scorer = Parsimony_Scorer("rooted_trees.txt","morph_data.nex")
--- parsimony_tree.py
+++ parsimony_tree.py
@@ -8,76 +8,76 @@ class Parsimony_Tree:
 		"internal" value to name the internal nodes. If no 
 		number is given, it assumes that the tree will
 		have 6 leaves (with internal starting at 7)'''
-		self.__root = None
-		self.__newick = newick
-		self.__internal = internal
-		self.__leaf_num = 0
-		self.__leaves= []
-		self.__taxa_list = self.__set_taxa_list()
+		self._root = None
+		self._newick = newick
+		self._internal = internal
+		self._leaf_num = 0
+		self._leaves= []
+		self._taxa_list = self._set_taxa_list()
 
-		self.__populate_parsimony_tree()
+		self._populate_parsimony_tree()
 
-	def __set_taxa_list(self):
+	def _set_taxa_list(self):
 		'''Sets the taxa list to hold the names of the taxa in the 
 		order they appear in the newick string'''
 		removals = ['(',')',',',';']
-		taxa_string = self.__newick
+		taxa_string = self._newick
 		for char in removals:
 			taxa_string = taxa_string.replace(char,"")
 
 		return taxa_string.split()
 
-	def __populate_parsimony_tree(self):
+	def _populate_parsimony_tree(self):
 		'''Builds the tree from the newick string. 
 		Assumes your newick string contains a space after each 
 		comma and splits it accordingly. Change what's in split's 
 		()'s below to match your newick string.'''
-		newick = self.__newick.split(', ')
-		self.__root, newick = self.__add_node(newick)
+		newick = self._newick.split(', ')
+		self._root, newick = self._add_node(newick)
 
-	def __add_node(self, newick):
+	def _add_node(self, newick):
 		'''Recursive function that builds an entire tree from a 
 		newick string. If chars data was given, it will also set 
 		the leaf character states'''
 		
 		if newick[0][0] == "(":
 			#must create an internal node
-			node = Parsimony_Node(self.__internal)
-			self.__internal += 1
+			node = Parsimony_Node(self._internal)
+			self._internal += 1
 			
 			#get rid of the '(' and create the internal node's left child
 			newick = [newick[0][1:]]+newick[1:]
-			node.left, newick = self.__add_node(newick)
+			node.left, newick = self._add_node(newick)
 			#get rid of the left child and create the right
-			node.right, newick = self.__add_node(newick[1:])
+			node.right, newick = self._add_node(newick[1:])
 		else:
-			node = self.__create_leaf_node()
+			node = self._create_leaf_node()
 
 		return node, newick
 
-	def __create_leaf_node(self):
+	def _create_leaf_node(self):
 		'''Builds a leaf node using the taxa-list and characters
 		if they exist'''
-		node = Parsimony_Node(self.__taxa_list[self.__leaf_num])
-		self.__leaf_num += 1	
-		self.__leaves.append(node)
+		node = Parsimony_Node(self._taxa_list[self._leaf_num])
+		self._leaf_num += 1	
+		self._leaves.append(node)
 		return node
 
 	def add_leaf_states(self,chars):
 		'''The same tree will need to be scored for multiple characters.
 		This resets the leaves' character states from a new dict of data.'''
-		for node in self.__leaves:
+		for node in self._leaves:
 			node.state = chars[node.name]
 
 	def get_post_order_nodes(self):
 		'''Returns list of nodes in the tree in post-order traversal.'''
-		self.__post_order_nodes = []
-		self.__post_order_traversal_2(self.__root)
-		return self.__post_order_nodes
+		self._post_order_nodes = []
+		self._post_order_traversal_2(self._root)
+		return self._post_order_nodes
 
-	def __post_order_traversal_2(self,current_node):
+	def _post_order_traversal_2(self,current_node):
 		'''continues until the current node is null'''
 		if current_node:
-			self.__post_order_traversal_2(current_node.left)
-			self.__post_order_traversal_2(current_node.right)
-			self.__post_order_nodes.append(current_node)
+			self._post_order_traversal_2(current_node.left)
+			self._post_order_traversal_2(current_node.right)
+			self._post_order_nodes.append(current_node)
